7ba07b2447440c9aca8f814e9cce4c365187ec16
Jjoobob123 <273hur4747@gmail.com>
2023-04-15 22:20:55 +0900

    기초 폴더 수정

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty({ example: '주현', description: '이름', required: true })
-  @IsString()
-  name: string;
+	@ApiProperty({ example: '주현', description: '이름', required: true })
+	@IsString()
+	name: string;
 
-  @ApiProperty({ example: 12, description: '나이', required: true })
-  @IsNumber()
-  age: number;
+	@ApiProperty({ example: 12, description: '나이', required: true })
+	@IsNumber()
+	age: number;
 }
